794521360975518932919f5df2e9808781cbff66
joshming <josh.jm2011@gmail.com>
2023-11-29 21:38:26 -0500

    fix

--- src/main/java/meerkat/mango/api/gateway/services/search/SearchResponse.java
+++ src/main/java/meerkat/mango/api/gateway/services/search/SearchResponse.java
@@ -1,6 +1,17 @@
 package meerkat.mango.api.gateway.services.search;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+@NoArgsConstructor(force = true)
 public class SearchResponse {
 
-
+    @JsonProperty
+    private final List<Product> products;
 }
