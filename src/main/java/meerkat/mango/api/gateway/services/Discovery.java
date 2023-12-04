c9d5a890928b979614f709b8ce3d4d3308b3ccb1
Joshua Ming <60520554+joshming@users.noreply.github.com>
2023-12-03 21:39:23 -0500

    Mango 82 (#14)
    * add specific methods for cors
    
    * git was bugging

--- src/main/java/meerkat/mango/api/gateway/services/Discovery.java
+++ src/main/java/meerkat/mango/api/gateway/services/Discovery.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -16,19 +17,28 @@ import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
 public class Discovery {
 
     private static final Logger LOG = LoggerFactory.getLogger(Discovery.class);
     private final ServiceRegistryService serviceRegistryService;
+    private final ExecutorService executorService;
+    private final Map<ServiceType, String> urlCache;
 
     @Autowired
     public Discovery(final ServiceRegistryService serviceRegistryService) {
         this.serviceRegistryService = serviceRegistryService;
+        this.executorService = Executors.newSingleThreadExecutor();
+        this.urlCache = new HashMap<>();
     }
 
     public String getService(final ServiceType type) {
+        if (urlCache.containsKey(type)) {
+            return urlCache.get(type);
+        }
         final var services = serviceRegistryService.verifyService(type.getServiceName());
 
         if (services == null || services.getServiceProviders() == null || services.getServiceProviders().isEmpty()) {
@@ -42,5 +52,25 @@ public class Discovery {
                     .port(serviceUrl.getPort());
             return "http:" + uri.toUriString();
         }).findAny().get();
+    }
+
+    @Scheduled(fixedDelay = 30000)
+    private void loadCache() {
+        LOG.debug("reloading cache");
+        for (final var type : ServiceType.values()) {
+            final var services = serviceRegistryService.verifyService(type.getServiceName());
+
+            if (services == null || services.getServiceProviders() == null || services.getServiceProviders().isEmpty()) {
+                LOG.error("No service providers for {} are available.", type.getServiceName());
+                continue;
+            }
+
+            urlCache.put(type, services.getServiceProviders().values().stream().map(serviceUrl -> {
+                final var uri = UriComponentsBuilder.newInstance()
+                        .host(serviceUrl.getIp())
+                        .port(serviceUrl.getPort());
+                return "http:" + uri.toUriString();
+            }).findAny().get());
+        }
     }
 }
