0ee598f0eca39a27527e050217db8a566e9cfaa0
Nicolas Jacobs <njacobs6@uwo.ca>
2023-12-06 19:18:59 -0500

    adding checkout controller and service

---
+++ src/main/java/meerkat/mango/api/gateway/services/checkout/CheckoutController.java
@@ -0,0 +1,30 @@
+package meerkat.mango.api.gateway.services.checkout;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/checkout")
+public class CheckoutController {
+
+    private final ProductsDao productsDao;
+
+    @Autowired
+    public CheckoutController(final ProductsDao productsDao) {
+        this.productsDao = productsDao;
+    }
+
+    @GetMapping(value = "/lower-stock/{productId}/{provider}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Boolean lowerStock(@PathVariable("productId") final String productId,
+                              @PathVariable("provider") final String provider,
+                              @RequestParam("amount") final int amount) {
+        return productsDao.lowerStock(productId, provider, amount);
+    }
+}
---
+++ src/main/java/meerkat/mango/api/gateway/services/checkout/CheckoutService.java
@@ -0,0 +1,45 @@
+package meerkat.mango.api.gateway.services.checkout;
+
+import meerkat.mango.api.gateway.resttemplate.CustomClientHttpRequestFactory;
+import meerkat.mango.api.gateway.services.Discovery;
+import meerkat.mango.api.gateway.services.ServiceType;
+import meerkat.mango.api.gateway.services.favourites.Favourites;
+import meerkat.mango.api.gateway.services.favourites.FavouritesItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+public class CheckoutService
+{
+    private final RestTemplate restTemplate;
+    private final Discovery discovery;
+    private static final String CHECKOUT_PATH = "checkout";
+
+    @Autowired
+    public CheckoutService(final Discovery discovery) {
+        this.discovery = discovery;
+        this.restTemplate = new RestTemplateBuilder()
+                .setConnectTimeout(Duration.of(2, ChronoUnit.SECONDS))
+                .setReadTimeout(Duration.of(2, ChronoUnit.SECONDS))
+                .requestFactory(CustomClientHttpRequestFactory.class)
+                .build();
+    }
+
+    public Boolean lowerStock(final String productId, final String provider, final int amount) {
+        final var url = discovery.getService(ServiceType.REVIEWS);
+        if (url == null) {
+            return null;
+        }
+
+        final var properUrl = UriComponentsBuilder.fromHttpUrl(url).pathSegment(CHECKOUT_PATH, productId, provider).build();
+
+        return restTemplate.getForEntity(properUrl.toUri(), Boolean.class).getBody();
+    }
+}
